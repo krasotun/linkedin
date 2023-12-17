1ccaf83d46edc0c6b45f8ce57e5afd3f4ac4c103
krasotun <servrostov@gmail.com>
2023-12-17 19:52:06 +0300

    add: components

--- linkedin/src/app/home/home.module.ts
+++ linkedin/src/app/home/home.module.ts
@@ -7,9 +7,19 @@ import { HomePage } from './home.page';
 import { HomePageRoutingModule } from './home-routing.module';
 import { HeaderComponent } from './components/header/header.component';
 import { PopoverComponent } from './components/header/popover/popover.component';
+import { ProfileSummaryComponent } from './components/profile-summary/profile-summary.component';
+import { StartPostComponent } from './components/start-post/start-post.component';
+import { AdvertisingComponent } from './components/advertising/advertising.component';
 
 @NgModule({
   imports: [CommonModule, FormsModule, IonicModule, HomePageRoutingModule],
-  declarations: [HomePage, HeaderComponent, PopoverComponent],
+  declarations: [
+    HomePage,
+    HeaderComponent,
+    PopoverComponent,
+    ProfileSummaryComponent,
+    StartPostComponent,
+    AdvertisingComponent,
+  ],
 })
 export class HomePageModule {}
