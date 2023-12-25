c72d86b6d5d6db3311ded141628c295b9fc1c5eb
krasotun <servrostov@gmail.com>
2023-12-25 20:01:10 +0300

    add a lot of

--- api/src/feed/models/post.interface.ts
+++ api/src/feed/models/post.interface.ts
@@ -1,5 +1,8 @@
+import { User } from './user.interface';
+
 export interface FeedPost {
   id?: number;
   body?: string;
   createdAt?: Date;
+  author?: User;
 }
---
+++ api/src/feed/models/user.interface.ts
@@ -0,0 +1,12 @@
+import { Role } from 'src/auth/models/role.enum';
+import { FeedPost } from './post.interface';
+
+export interface User {
+  id?: number;
+  firstName?: string;
+  lastName?: string;
+  email?: string;
+  password?: string;
+  role?: Role;
+  posts?: FeedPost[];
+}
